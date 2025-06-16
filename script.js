a1fb9ab350686bdc75b0a3d08f4c44f5f0c5a791
kehinde2008 <keke.afolabi25@gmail.com>
2025-06-16 10:07:48 -0700

    home

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 // Declare variables below to save the different divs of your story.
+let picOne = document.querySelector(".option-one");
+let boxOne = document.querySelector(".option-one-screen");
 
+let picTwo = document.querySelector(".option-two");
+let boxTwo =document.querySelector(".option-two-screen");
 
+let nextStep =document.querySelector(".nextStep");
 
+let jump =document.querySelector(".option-three");
+let jump2 =document.querySelector(".option-four");
 
+let draw =document.querySelector(".option-one-end");
+let draw2 =document.querySelector(".option-two-end");
 
-
-
-
+let con =document.querySelectorAll(".congrats");
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+picOne.addEventListener('click', function(){
+    boxOne.style.display="block";
+    nextStep.style.display="block";
+    jump.style.display="block";
+    jump2.style.display="block";
+});
 
-// });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+picTwo.addEventListener('click', function(){
+    boxTwo.style.display="block";
+    nextStep.style.display="block";
+    jump.style.display="block";
+    jump2.style.display="block";
+ });
 
-// });
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+jump.addEventListener('click', function(){
+    draw.style.display="block";
+    
+});
 
-// });
+jump2.addEventListener('click', function(){
+    draw2.style.display="block";
+
+})
